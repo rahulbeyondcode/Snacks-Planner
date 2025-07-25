547fa749726acd430e1616303103f37a6904f9e7
Ramsiya <ramsiya@quintetsolutions.com>
2025-07-25 12:55:55 +0530

    remove duplicate entry

--- Backend/database/migrations/2025_07_23_000108_create_money_pools_table.php
+++ Backend/database/migrations/2025_07_23_000108_create_money_pools_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->timestamps();
             $table->softDeletes();
             $table->foreign('created_by')->references('user_id')->on('users')->onDelete('cascade');
-            $table->softDeletes();
         });
     }
 
