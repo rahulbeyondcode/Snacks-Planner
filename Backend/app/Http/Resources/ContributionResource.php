db5801917b0aea94c76b9a99ccba819bfa704631
Ajai Mathew <ajaimathew@quintetsolutions.com>
2025-09-25 16:21:20 +0530

    Change the contribution status

--- Backend/app/Http/Resources/ContributionResource.php
+++ Backend/app/Http/Resources/ContributionResource.php
@@ -12,7 +12,7 @@ class ContributionResource extends JsonResource
             'contribution_id' => $this->contribution_id,
             'user_id' => $this->user_id,
             'user_name' => $this->user ? $this->user->name : null,
-            'status' => $this->status === 'paid',
+            'status' => $this->status,
         ];
     }
 }
